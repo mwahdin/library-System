107580a0d3b52d161484d15ef373812f9289e617
mahdiRahimzadeh <rahimzadehm65@gmail.com>
2025-04-10 18:43:00 +0330

    book adding

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,18 @@ public class Main {
 
         Book book1 = new Book("Java Programming", "John Doe", 1980);
         Book book2 = new Book("Data Structures", "jane Smith", 1994);
-        Book book3 = new Book("agant", "", 1980);
+        Book book3 = new Book("agant", "rasell", 1980);
 
         library.addBook(book1);
         library.addBook(book2);
+        library.addBook(book3);
 
         User user1 = new User("mahdi Rahimzadeh", "sdf4f5d", "rahimzadehm65@gmail.com", "09395582443", "mard", 23);
 
         user1.borrowBook(book1);
         user1.borrowBook(book2);
         user1.returnBook(book1);
+        user1.borrowBook(book3);
         library.displayAllBooks();
 
     }
