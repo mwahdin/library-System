6553b22a56c6224a6c85b42b79d9c08ba7c30920
mahdiRahimzadeh <rahimzadehm65@gmail.com>
2025-04-13 22:21:12 +0330

    edit main java class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Library library = new Library();
 
-        Book book1 = new Book("Java Programming", "John Doe", 1980);
-        Book book2 = new Book("Data Structures", "jane Smith", 1994);
-        Book book3 = new Book("agant", "rasell", 1980);
-
-        library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
 
