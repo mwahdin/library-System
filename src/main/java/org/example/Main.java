97072f14b5eb415c709067b056130bef5543acec
mahdiRahimzadeh <rahimzadehm65@gmail.com>
2025-04-13 22:19:35 +0330

    remove user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,6 @@ public class Main {
         library.addBook(book2);
         library.addBook(book3);
 
-        User user1 = new User("mahdi Rahimzadeh", "sdf4f5d", "rahimzadehm65@gmail.com", "09395582443", "mard", 23);
 
         user1.borrowBook(book1);
         user1.borrowBook(book2);
